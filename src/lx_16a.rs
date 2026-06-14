3373ce725f36e0c36273adc532851fcadc93a40d
David Wheeler <DaveOfThings@gmail.com>
2026-06-14 10:41:10 -0400

    Refactored LX16A stuff, starting to add more commands.

--- src/lx_16a.rs
+++ src/lx_16a.rs
@@ -1,89 +1,213 @@
-use std::{io::Error, sync::Mutex};
-use serialport::SerialPort;
+use std::{io::{Error, ErrorKind::InvalidData, Read, Write}, sync::Mutex};
+use byteorder::{ByteOrder, LittleEndian};
 
 // TODO-DW : Look into making T embeddedio::Read and embeddedio::Write instead of SerialPort
 
-// LX-16A Command bytes
-const SERVO_ID_READ: u8 = 14;
-
-pub struct SerialBus {
-    port: Mutex<Box<dyn SerialPort>>,
+// TODO-DW : Add support for direction control?
+pub struct Lx16aBus<T: Read+Write> {
+    port: Mutex<T>,
 }
 
-impl SerialBus {
-    pub fn new(port: Box<dyn SerialPort>) -> SerialBus {
-        SerialBus { port: Mutex::new(port) }
+impl<'a, T: Read+Write> Lx16aBus<T> {
+    // Create an Lx16a bus from anything that implements Read+Write
+    pub fn new(port: T) -> Lx16aBus<T> {
+        Lx16aBus { port: Mutex::new(port) }
+    }
+
+    // Get the "broadcast" servo
+    pub fn broadcast(&'a self) -> Servo<'a, T> {
+        const BROADCAST_ID: u8 = 254;
+
+        Servo::new(BROADCAST_ID, self)
+    }
+
+    // Get an individual servo
+    pub fn servo(&'a self, id: u8) -> Servo<'a, T> {
+        Servo::new(id, self)
     }
 
     // TODO: Create status return value
     // Write data
-    pub fn write(&self, out_data: &[u8]) {
-        // TODO
-    }
-
-    // TODO: Create status return value
-    // Write data then read expected response.
-    pub fn write_read(&self, out_data: &[u8], rx_data: &mut[u8]) -> Result<usize, Error> {
-        // Get access to the bus
+    pub fn write(&self, out_data: &[u8])  -> Result<(), Error> {
+        // Get exclusive access to the bus
         let mut port = self.port.lock().unwrap();
 
         // write the request
-        print!("Write:");
-        out_data.iter().for_each(|byte| {print!(" {byte:02x}")});
-        println!("");
+        port.write(out_data)?;
+        port.flush()?;
 
+        Ok(())
+    }
+
+    // Write data then read expected response.
+    pub fn write_read(&self, out_data: &[u8], rx_data: &mut[u8]) -> Result<usize, Error> {
+        // Get exclusive access to the bus
+        let mut port = self.port.lock().unwrap();
+
+        // write the request
         port.write(out_data)?;
         port.flush()?;
 
         // read the response
         port.read_exact(rx_data)?;
 
-        print!("Read:");
-        rx_data.iter().for_each(|byte| {print!(" {byte:02x}")});
-        println!("");
-
         Ok(rx_data.len())
     }
 }
 
 // Implement interface to one LX-16a servo on a bus.
-pub struct Servo<'a> {
+pub struct Servo<'a, T: Read+Write> {
     id: u8, 
-    bus: &'a SerialBus,
+    bus: &'a Lx16aBus<T>,
 }
 
-impl<'a> Servo<'a> {
-    pub fn new(id: u8, bus: &'a SerialBus) -> Servo<'a> {
+impl<'a, T: Read+Write> Servo<'a, T> {
+    // Private new method, used by Lx16aBus.
+    // To create a servo, first create an Lx16aBus, then use servo() factory method.
+    fn new(id: u8, bus: &'a Lx16aBus<T>) -> Servo<'a, T> {
         Servo { id, bus }
     }
 
+    // Get the ID associated with this servo
     pub fn get_id(&self) -> u8 {
         self.id
     }
 
-    pub fn set_checksum(msg: &mut [u8]) {
-        // Add the payload bytes together, mod 256, and negate.
-        let len = msg.len();
-        let sum = msg[2..len-1].iter().sum::<u8>() ^ 0xFF;
+    // --- Public operations corresponding to servo commands
 
-        // Stuff the result into the last byte of the message.
-        msg[len-1] = sum;
+    // TODO : Move LX16a stuff to its own library crate
+    #[allow(unused)] 
+    pub fn move_time(&self, pos: i16, time_ms: u16) -> Result<(), Error> {
+        const SERVO_MOVE_TIME_WRITE: u8 = 1;
+
+        let mut params = [0; 4];
+        LittleEndian::write_i16(&mut params[0..2], pos);
+        LittleEndian::write_u16(&mut params[2..4], time_ms);
+        self.write(self.id, SERVO_MOVE_TIME_WRITE, &params)?;
+
+        Ok(())
+    }
+    
+    pub fn move_wait(&self, pos: i16, time_ms: u16) -> Result<(), Error> {
+        const SERVO_MOVE_TIME_WAIT_WRITE: u8 = 7;
+
+        let mut params = [0; 4];
+        LittleEndian::write_i16(&mut params[0..2], pos);
+        LittleEndian::write_u16(&mut params[2..4], time_ms);
+        self.write(self.id, SERVO_MOVE_TIME_WAIT_WRITE, &params)?;
+
+        Ok(())
     }
 
-    pub fn read_servo_id(&self) -> u8 {
-        // Construct the query
-        let mut query = [0x55, 0x55, self.id, 3, SERVO_ID_READ, 0x00];
-        Self::set_checksum(&mut query);
+    pub fn move_start(&self) -> Result<(), Error> {
+        const SERVO_MOVE_START: u8 = 11;
 
-        let mut response = [0x00; 7];
+        let params = [];
+        self.write(self.id, SERVO_MOVE_START, &params)?;
 
-        self.bus.write_read(&query, &mut response);
+        Ok(())
+    }
 
-        // TODO : Validate response (header, id, command, len, checksum)
+    pub fn read_servo_id(&self) -> Result<u8, Error> {
+        const SERVO_ID_READ: u8 = 14;
 
-        // TODO : Extract ID from response
+        let mut rx_buf = [0; 32];
+        let response = self.read(self.id, SERVO_ID_READ, &[], &mut rx_buf, 1)?;
 
-        // TODO : Return id
-        response[5]
+        Ok(response[0])
+    }
+
+    pub fn read_temp_c(&self) -> Result<i8, Error> {
+        const SERVO_TEMP_READ: u8 = 26;
+
+        let mut rx_buf = [0; 32];
+        let response = self.read(self.id, SERVO_TEMP_READ, &[], &mut rx_buf, 1)?;
+
+        Ok(response[0] as i8)
+    }
+
+    pub fn read_vin_mv(&self) -> Result<i16, Error> {
+        const SERVO_VIN_READ: u8 = 27;
+
+        let mut rx_buf = [0; 32];
+        let response = self.read(self.id, SERVO_VIN_READ, &[], &mut rx_buf, 2)?;
+
+        Ok(LittleEndian::read_i16(&response[0..2]))
+    }
+
+    pub fn read_pos(&self) -> Result<i16, Error> {
+        const SERVO_POS_READ: u8 = 28;
+
+        let mut rx_buf = [0; 32];
+        let response = self.read(self.id, SERVO_POS_READ, &[], &mut rx_buf, 2)?;
+
+        Ok(LittleEndian::read_i16(&response[0..2]))
+    }
+
+    // --- Utility methods --------------------------------------------
+    
+    fn checksum(msg: &[u8]) -> u8 {
+        // Add the payload bytes together, mod 256, and negate.
+        let sum = msg.iter()
+            .fold(0_u8, |acc, &x| { 
+                acc.wrapping_add(x)
+            }) ^ 0xFF;
+
+        // Stuff the result into the last byte of the message.
+        sum
+    }
+
+    fn form_packet(tx_data: &mut[u8], id: u8, cmd: u8, params: &[u8]) -> usize {
+        let mut len;
+        let len_field = 3_u8 + params.len() as u8;
+
+        // Format contents of packet in tx_data
+        tx_data[0] = 0x55;
+        tx_data[1] = 0x55;
+        tx_data[2] = id;
+        tx_data[3] = len_field;
+        tx_data[4] = cmd;
+        len = 5;
+        params.iter()
+            .for_each(|b| { 
+                tx_data[len] = *b; 
+                len += 1; 
+            });
+        tx_data[len] = Self::checksum(&tx_data[2..len]);
+        len += 1;
+
+        len
+    }
+
+    fn write(&self, id: u8, cmd: u8, params: &[u8]) -> Result<(), Error> {
+        let mut tx_data: [u8; 32] = [0; 32];
+        let tx_data_len = Self::form_packet(&mut tx_data, id, cmd, params);
+
+        self.bus.write(&tx_data[0..tx_data_len])?;
+
+        Ok(())
+    }
+
+    fn read(&self, id: u8, cmd: u8, params: &[u8], rx_buf: &'a mut[u8], params_len: usize) -> Result<&'a [u8], Error> {
+        let mut tx_data: [u8; 32] = [0; 32];
+        let tx_data_len = Self::form_packet(&mut tx_data, id, cmd, params);
+        let resp_len = params_len + 6;
+
+        self.bus.write_read(&tx_data[0..tx_data_len], &mut rx_buf[0..resp_len])?;
+
+        // Validate response: header, id, length, cmd, checksum
+        if Self::checksum(&rx_buf[2..resp_len]) != 0_u8 {
+            Err(Error::new(InvalidData, "Checksum error"))
+        }
+        else if (rx_buf[0] != 0x55) ||
+           (rx_buf[1] != 0x55) ||
+           (rx_buf[2] != id) ||
+           (rx_buf[3] != 3_u8 + params_len as u8) ||
+           (rx_buf[4] != cmd) {
+            Err(Error::new(InvalidData, "Bad response"))
+        }
+        else {
+            Ok(&rx_buf[5..5+params_len])
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-// use lx16a_servo::{ServoBus, Servo, ServoID};
-
 mod lx_16a;
 
 use std::time::Duration;
-
-use serialport::SerialPort;
-use lx_16a::{SerialBus, Servo};
+use lx_16a::Lx16aBus;
 
 const SERIAL_PORT: &str = "/dev/ttyUSB0";
 const BAUD: u32 = 115200;
@@ -15,10 +11,10 @@ const RIGHT_BACK_STEER_ID: u8 = 4;
 const LEFT_BACK_STEER_ID: u8 = 6;
 const LEFT_FRONT_STEER_ID: u8 = 9;
 
-const RIGHT_FRONT_STRAIGHT: u32 = 397;
-const RIGHT_BACK_STRAIGHT: u32 = 509;
-const LEFT_BACK_STRAIGHT: u32 = 447;
-const LEFT_FRONT_STRAIGHT: u32 = 564;
+// const RIGHT_FRONT_STRAIGHT: u32 = 397;
+// const RIGHT_BACK_STRAIGHT: u32 = 509;
+// const LEFT_BACK_STRAIGHT: u32 = 447;
+// const LEFT_FRONT_STRAIGHT: u32 = 564;
 
 fn main() -> anyhow::Result<()> {
     println!("Hello, world!");
@@ -27,17 +23,27 @@ fn main() -> anyhow::Result<()> {
         .timeout(Duration::from_millis(10))
         .open()
         .expect("Failed to open port");
-    let serial_bus = SerialBus::new(port);
+    let lx16a_bus = Lx16aBus::new(port);
 
-    let right_front = Servo::new(RIGHT_FRONT_STEER_ID, &serial_bus);
-    let right_back  = Servo::new(RIGHT_BACK_STEER_ID,  &serial_bus);
-    let left_back   = Servo::new(LEFT_BACK_STEER_ID,   &serial_bus);
-    let left_front  = Servo::new(LEFT_FRONT_STEER_ID,  &serial_bus);
+    let right_front = lx16a_bus.servo(RIGHT_FRONT_STEER_ID);
+    let right_back  = lx16a_bus.servo(RIGHT_BACK_STEER_ID);
+    let left_back   = lx16a_bus.servo(LEFT_BACK_STEER_ID);
+    let left_front  = lx16a_bus.servo(LEFT_FRONT_STEER_ID);
     
     for servo in [&right_front, &right_back, &left_back, &left_front] {
-    	let read_id = servo.read_servo_id();
-        println!("Servo {} reports id {read_id}", servo.get_id());
+    	let read_id = servo.read_servo_id().unwrap();
+        let temp = servo.read_temp_c().unwrap();
+        let pos = servo.read_pos().unwrap();
+        let vin_mv = servo.read_vin_mv().unwrap();
+        println!("Servo {}:", servo.get_id());
+        println!("    id :{read_id}, temp: {temp}, position: {pos}, vin [mv]: {vin_mv}");
+
+        // Move to position 500 over 1 sec.
+        servo.move_wait(500, 1000)?;
     }
+
+    // start the move
+    lx16a_bus.broadcast().move_start()?;
 
     Ok(())
 }
